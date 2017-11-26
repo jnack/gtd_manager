b740f0b8b897f60cf1bb9716bb719ba6235c117c
jnack <jason.j.nack@gmail.com>
2017-11-26 15:46:28 -0500

    Created templates and views to create task list.

--- taskManager/views.py
+++ taskManager/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Task, Project
+from django.utils import timezone
 
 # Create your views here.
+def task_list(request):
+    tasks = Task.objects.filter(stage = 'prep').order_by('due_date')
+    return render(request, 'taskManager/task_list.html', {'tasks': tasks})
